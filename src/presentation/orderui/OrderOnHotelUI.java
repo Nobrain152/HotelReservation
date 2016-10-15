6236971064560f07ce5cfba3cbfe095330e9d060
txin15 <995472235@qq.com>
2016-10-15 17:46:04 +0800

    orderui

---
+++ src/presentation/orderui/OrderOnHotelUI.java
@@ -0,0 +1,14 @@
+package presentation.orderui;
+/**
+ * ¾Æµê¶©µ¥
+ * @author txin
+ * @data 2016/10/15
+ *
+ */
+import businessorderservice.orderblservice.OrderOnHotelBLService;
+
+public class OrderOnHotelUI {
+
+	public OrderOnHotelUI(OrderOnHotelBLService service){}
+	
+}
---
+++ src/presentation/orderui/OrderOnWebUI.java
@@ -0,0 +1,14 @@
+package presentation.orderui;
+/**
+ * ÍøÕ¾¶©µ¥
+ * @author txin
+ * @data 2016/10/15
+ *
+ */
+import businessorderservice.orderblservice.OrderOnWebBLService;
+
+public class OrderOnWebUI {
+
+	public OrderOnWebUI(OrderOnWebBLService service){}
+	
+}
