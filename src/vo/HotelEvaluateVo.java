52f7e2d04eff1478ea3601d42e1911093dff0477
guyifei <943045598@qq.com>
2016-10-16 22:12:53 +0800

    ？？？

--- src/vo/HotelEvaluateVo.java
+++ src/vo/HotelEvaluateVo.java
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-package vo;
-
-public class HotelEvaluateVO{
-
-	/**
-	 * 评分
-	 */
-	private int score;
-
-	/**
-	 * 评论信息
-	 */
-	private String comment;
-	
-	/**
-	 * 是否订过
-	 */
-	private boolean reserve;
-	
-	
-	public HotelEvaluateVO(int score, String comment, boolean reserve) {
-		super();
-		this.score = score;
-		this.comment = comment;
-		this.reserve=reserve;
-	}
-
-	public String getScore() {
-		return score;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public boolean isReserved(){
-		return reserve;
-	}
-	
-=======
 package vo;
 
 
@@ -80,5 +39,4 @@ public class HotelEvaluateVO{
 		return reserve;
 	}
 	
->>>>>>> 727ede01d10abd2e000e306ffc37ab3c9bba25f7
 }	
